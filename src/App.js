8ed39983c61b202dc91ca38852bf1289f50a7a1f
Adrian Wilk <wilku5991@wp.pl>
2021-06-09 20:47:11 +0200

    addNewTask interaction

--- src/App.js
+++ src/App.js
@@ -38,6 +38,17 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTask(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ])
+  };
+
   return (
     <Container>
       <Header
@@ -47,7 +58,7 @@ function App() {
       <Section
         title="Dodaj nowe zadanie"
         body={
-          <Form />
+          <Form addNewTask={addNewTask} />
         }
       />
 
