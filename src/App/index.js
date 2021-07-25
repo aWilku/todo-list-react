d3a99d8fe127e222c2c9ac516afa28b50d26d003
Adrian Wilk <wilku5991@wp.pl>
2021-07-25 16:39:09 +0200

    add theme.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useTask } from "./useTasks";
+import { theme } from "../Theme"
+import { ThemeProvider } from "styled-components";
 import Buttons from "./Container/Section/Buttons";
 import Form from "./Container/Section/Form";
 import Header from "./Container/Header";
@@ -23,38 +25,40 @@ function App() {
   } = useTask();
 
   return (
-    <Container>
-      <Header
-        title="Lista zadań"
-      />
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Header
+          title="Lista zadań"
+        />
 
-      <Section
-        title="Dodaj nowe zadanie"
-        body={
-          <Form addNewTask={addNewTask} />
-        }
-      />
+        <Section
+          title="Dodaj nowe zadanie"
+          body={
+            <Form addNewTask={addNewTask} />
+          }
+        />
 
-      <Section
-        title="Lista zadań"
-        body={
-          <Tasks
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
-      />
-    </Container>
+        <Section
+          title="Lista zadań"
+          body={
+            <Tasks
+              tasks={tasks}
+              hideDone={hideDone}
+              removeTask={removeTask}
+              toggleTaskDone={toggleTaskDone}
+            />
+          }
+          extraHeaderContent={
+            <Buttons
+              tasks={tasks}
+              hideDone={hideDone}
+              toggleHideDone={toggleHideDone}
+              setAllDone={setAllDone}
+            />
+          }
+        />
+      </Container>
+    </ThemeProvider>
   );
 };
 export default App;
